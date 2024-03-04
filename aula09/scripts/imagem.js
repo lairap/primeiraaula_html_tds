a4f18a812d7e2260d523a9b803fbdefe14b1019d
lairap <lairaluizapires@gmail.com>
2024-03-04 11:51:52 -0300

    oitavo commit java galeria

---
+++ aula09/scripts/imagem.js
@@ -0,0 +1,26 @@
+let images = [];
+
+function carregarImagem(){
+    const galeria = document.getElementById("galeria");
+    galeria.innerHTML = '';
+
+    images.forEach(imgURL => {
+        const img = document.createElement ("img");
+        img.src = imgURL;
+        galeria.appendChild(img)
+    });
+}
+
+document.getElementById('arquivos').addEventListener('change', function(event){
+    const arquivo = event.target.files[0];
+    const ler = new FileReader();
+
+    ler.onload = function(e){
+        const imgURL = e.target.result;
+        images.push(imgURL);
+        carregarImagem();
+
+    }
+
+    ler.readAsDataURL(arquivo)
+});
